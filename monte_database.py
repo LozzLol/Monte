29566c1dce0deaebd41220a4e8c747ba0e754119
louis <thelouisman@hotmail.co.uk>
2019-04-08 20:21:21 +0100

    updates

--- monte_database.py
+++ monte_database.py
@@ -2,56 +2,140 @@ import sqlite3
 import json
 from datetime import datetime
 
-timeframe = "2006-09"
-# Used to hold the transaction so we can carry out all queries at once
+timeframe = '2006-01'
 sql_transaction = []
-# Connects to the database
+
 connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
 
-
 def create_table():
-    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply
-    (parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, 
-    comment TEXT, subreddit TEXT, unix INT, score INT)""")
+    c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
 
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        # print(str(e))
+        return False
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]':
+        return False
+    elif data == '[removed]':
+        return False
+    else:
+        return True
 
 def find_parent(pid):
-    # Finds the parent body of text
-    sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
-    c.execute(sql)
-    result = c.fetchone()
-    if result is not None:
-        return result[0]
-    else:
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print(str(e))
         return False
-    # except Exception as e:
-    # print("find_parent", e)
-    # return False
 
 
+# Sanitise some of the data
 def format_data(data):
-    # Sanitise some of the data
-    data = data.replace("\n", " newlinechar ").replace("\r", " newlinechar ").replace('"', "'")
+    data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
     return data
 
 
+def sql_insert_replace_comment(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id =?;""".format(parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s0 insertion',str(e))
+
+
+def sql_insert_has_parent(commentid, parentid, parent, comment, subreddit, time, score):
+    try:
+
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(
+            parentid, commentid, parent, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+
+    except Exception as e:
+        print('s0 insertion', str(e))
+
+
+def sql_insert_no_parent(commentid, parentid, comment, subreddit, time, score):
+    try:
+
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(
+            parentid, commentid, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+
+    except Exception as e:
+        print('s0 insertion', str(e))
+
+
+def transaction_bldr(sql):
+    global sql_transaction
+    sql_transaction.append(sql)
+    if len(sql_transaction) > 1000:
+        c.execute('BEGIN TRANSACTION')
+        for s in sql_transaction:
+            try:
+                c.execute(s)
+            except:
+                pass
+        connection.commit()
+        sql_transaction = []
+
+
 # Create table if it doesnt exist
-if __name__ == "__main__":
+if __name__ == '__main__':
     create_table()
     row_counter = 0
-    # how many relationships have we come up with
     paired_rows = 0
-    # Iterate through the file
-    with open("D:/Documents/reddit_comments/RC_{}".format(timeframe), buffering=1000) as f:
+
+    with open('RC_{}'.format(timeframe), buffering=1000) as f:
+        print(f)
         for row in f:
-            print(row)
-            row_counter +=1
+            # print(row)
+            row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
             body = format_data(row['body'])
             created_utc = row['created_utc']
             score = row['score']
+            comment_id = row['author']
             subreddit = row['subreddit']
-
             parent_data = find_parent(parent_id)
+
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id)
+                if existing_comment_score:
+                    if score > existing_comment_score:
+                        if acceptable(body):
+                            sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit, created_utc,
+                                                       score)
+
+                else:
+                    if acceptable(body):
+                        if parent_data:
+                            sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc,
+                                                  score)
+                            paired_rows += 1
+                        else:
+                            sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
+
+            if row_counter % 1000 == 0:
+                print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows,
+                                                                              str(datetime.now())))
